ca308fa682759c237e7540c21beef7200005ff60
전해윤 <junhaeyoon@gmail.com>
2021-09-06 17:45:42 +0900

    Fix: Log -> throw 로 변경

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,61 +1,46 @@
-import { logger } from '../lib/logger';
+import { AxiosError } from 'axios';
 
-export const GetTokenErrorHandler = async (status: number) => {
-	switch (status) {
+export const GetTokenErrorHandler = async (err: AxiosError) => {
+	switch (err.response?.data.status) {
 		case 400:
-			logger.error('검증오류');
-			break;
+			throw new Error('검증오류');
 		case 401:
-			logger.error('잘못된 clientSecret입니다');
-			break;
+			throw new Error('잘못된 clientSecret입니다');
 		case 403:
-			logger.error('변조된 code 입니다');
-			break;
+			throw new Error("변조된 code입니다");
 		case 410:
-			logger.error('토큰이 만료 되었습니다');
-			break;
+			throw new Error('토큰이 만료 되었습니다');
 		case 500:
-			logger.error('Open API 서버 오류');
-			break;
+			throw new Error('Open API 서버 오류');
 	}
 }
 
-export const RefreshTokenErrorHandler = async (status: number) => {
-	switch (status) {
+export const RefreshTokenErrorHandler = async (err: AxiosError) => {
+	switch (err.response?.status) {
 		case 400:
-			logger.error('잘못된 토큰입니다');
-			break;
+			throw new Error('잘못된 토큰입니다');
 		case 401:
-			logger.error('위조된 토큰입니다');
-			break;
+			throw new Error('위조된 토큰입니다');
 		case 404:
-			logger.error('찾을 수 없는 회원입니다');
-			break;
+			throw new Error('찾을 수 없는 회원입니다');
 		case 410:
-			logger.error('토큰이 만료 되었습니다');
-			break;
+			throw new Error('토큰이 만료 되었습니다');
 		case 500:
-			logger.error('Open API 서버 오류');
-			break;
+			throw new Error('Open API 서버 오류');
 	}
 }
 
-export const GetUserErrorHandler = async (status: number) => {
-	switch (status) {
+export const GetUserErrorHandler = async (err: AxiosError) => {
+	switch (err.response?.status) {
 		case 400:
-			logger.error('토큰이 전송되지 않았습니다');
-			break;
+			throw new Error('토큰이 전송되지 않았습니다');
 		case 401:
-			logger.error('위조된 토큰입니다');
-			break;
+			throw new Error('위조된 토큰입니다');
 		case 403:
-			logger.error('유저를 찾을 수 없습니다');
-			break;
+			throw new Error('유저를 찾을 수 없습니다');
 		case 410:
-			logger.error('토큰이 만료 되었습니다');
-			break;
+			throw new Error('토큰이 만료 되었습니다');
 		case 500:
-			logger.error('Open API 서버 오류');
-			break;
+			throw new Error('Open API 서버 오류');
 	}
 }
