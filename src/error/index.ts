f0ef6e16cd71caff6a7b76c63a912dcc1df11cff
Jerry <jeon@kakao.com>
2021-09-12 16:13:54 +0900

    feat: 오류 반환값에 오류 코드를 포함하도록 변경

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,46 +1,47 @@
 import { AxiosError } from 'axios';
+import { APIError } from '../types';
 
 export const GetTokenErrorHandler = async (err: AxiosError) => {
 	switch (err.response?.data.status) {
 		case 400:
-			throw new Error('검증오류');
+			throw new APIError(400, '검증오류');
 		case 401:
-			throw new Error('잘못된 clientSecret입니다');
+			throw new APIError(401, '잘못된 clientSecret입니다');
 		case 403:
-			throw new Error("변조된 code입니다");
+			throw new APIError(403, "변조된 code입니다");
 		case 410:
-			throw new Error('토큰이 만료 되었습니다');
+			throw new APIError(410, '토큰이 만료 되었습니다');
 		case 500:
-			throw new Error('Open API 서버 오류');
+			throw new APIError(500, 'Open API 서버 오류');
 	}
 }
 
 export const RefreshTokenErrorHandler = async (err: AxiosError) => {
 	switch (err.response?.status) {
 		case 400:
-			throw new Error('잘못된 토큰입니다');
+			throw new APIError(400, '잘못된 토큰입니다');
 		case 401:
-			throw new Error('위조된 토큰입니다');
+			throw new APIError(401, '위조된 토큰입니다');
 		case 404:
-			throw new Error('찾을 수 없는 회원입니다');
+			throw new APIError(404, '찾을 수 없는 회원입니다');
 		case 410:
-			throw new Error('토큰이 만료 되었습니다');
+			throw new APIError(410, '토큰이 만료 되었습니다');
 		case 500:
-			throw new Error('Open API 서버 오류');
+			throw new APIError(500, 'Open API 서버 오류');
 	}
 }
 
 export const GetUserErrorHandler = async (err: AxiosError) => {
 	switch (err.response?.status) {
 		case 400:
-			throw new Error('토큰이 전송되지 않았습니다');
+			throw new APIError(400, '토큰이 전송되지 않았습니다');
 		case 401:
-			throw new Error('위조된 토큰입니다');
+			throw new APIError(401, '위조된 토큰입니다');
 		case 403:
-			throw new Error('유저를 찾을 수 없습니다');
+			throw new APIError(403, '유저를 찾을 수 없습니다');
 		case 410:
-			throw new Error('토큰이 만료 되었습니다');
+			throw new APIError(410, '토큰이 만료 되었습니다');
 		case 500:
-			throw new Error('Open API 서버 오류');
+			throw new APIError(500, 'Open API 서버 오류');
 	}
 }
