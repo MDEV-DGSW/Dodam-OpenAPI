72f26c803aee86ee6e23ab6bfda3a29bfb8f6705
Jerry <jeon@kakao.com>
2021-09-12 16:13:24 +0900

    feat: 반환타입에 맞추어 변경

--- src/api.ts
+++ src/api.ts
@@ -1,44 +1,51 @@
 import axios from 'axios';
 import { GetTokenErrorHandler, GetUserErrorHandler, RefreshTokenErrorHandler } from './error';
 import { EndPoints } from './enum/EndPoints';
+import { GetTokenResponse, RefreshTokenResponse, GetUserResponse } from './types';
 
-export const getToken = async (code: string, clientId: string, clientSecret: string) => {
+export const getToken = async (code: string, client_id: string, client_secret: string): Promise<GetTokenResponse> => {
 	try {
 		return (await axios.post(`${EndPoints.DAUTH}`, {
 			code,
-			clientId,
-			clientSecret
+			client_id,
+			client_secret
 		})).data.data;
 	} catch (err) {
 		if (axios.isAxiosError(err)) {
 			GetTokenErrorHandler(err);
 		}
+
+		throw err;
 	}
 }
 
-export const refreshToken = async (refreshToken: string, clientId: string) => {
+export const refreshToken = async (refresh_token: string, client_id: string): Promise<RefreshTokenResponse> => {
 	try {
 		return (await axios.post(`${EndPoints.DAUTH}/refresh`, {
-			refreshToken,
-			clientId
+			refresh_token,
+			client_id
 		})).data.data;
 	} catch (err) {
 		if (axios.isAxiosError(err)) {
 			RefreshTokenErrorHandler(err);
 		}
+
+		throw err;
 	}
 }
 
-export const getUser = async (accessToken: string) => {
+export const getUser = async (access_token: string): Promise<GetUserResponse> => {
 	try {
 		return (await axios.get(`${EndPoints.OPENAPI}`, {
 			headers: {
-				'access-token': accessToken
+				'Authorization': `Bearer ${access_token}`
 			}
 		})).data.data;
 	} catch (err) {
 		if (axios.isAxiosError(err)) {
 			GetUserErrorHandler(err)
 		}
+
+		throw err;
 	}
 }
